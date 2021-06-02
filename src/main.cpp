5a7b5726f6254c0146274408bfad9d4f1cc58f2f
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-06-01 23:54:02 -0600

    ✨ FEAT: Use new system creation in main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,20 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <cstdint>
-#include <entt/entt.hpp>
 #include <iostream>
 
 #include "../lib/component/position.hpp"
 #include "../lib/component/velocity.hpp"
 #include "../lib/render/GraphicsGame.hpp"
+#include "../lib/system/velocitySystem.hpp"
 
-static entt::registry registry;
 static std::uint64_t dt = 0;
 static std::uint64_t lastTime = 0;
 static GraphicsGame game("Crucible", 600, 400);
 
 int main() {
     game.init();
+    game.addSystem<VelocitySystem>();
     while (game.isRunning()) {
         game.update(0);
     }

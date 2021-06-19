3512f4c877969fcf4fea864364dc1130f3c5fab9
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-06-18 18:33:26 -0600

    in progress commit

---
+++ lib/component/RectangleRenderer.hpp
@@ -0,0 +1,10 @@
+#ifndef CRUCIBLE_LIB_COMPONENT_RECTANGLERENDERER_HPP_
+#define CRUCIBLE_LIB_COMPONENT_RECTANGLERENDERER_HPP_
+
+#include <sfml/Graphics.hpp>
+
+struct RectangleRenderer {
+    sf::RectangleShape rectangle;
+};
+
+#endif  // CRUCIBLE_LIB_COMPONENT_RECTANGLERENDERER_HPP_
